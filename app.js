78ce1e6c615963d20b83fc8a03bb0031e0e37bfb
Joel Ford <Joel@Joels-MacBook-Pro.local>
2018-10-24 14:30:56 -0500

    test

---
+++ app.js
@@ -0,0 +1,21 @@
+// array of friends
+let friends =["Josh", "Joel", "Kelsy", "Devin", "Nathan"];
+
+//Function that logs 99 lines of code song
+
+for (i=0; i<friends.length; i++){
+    console.log(friends[i])
+    let friend = friends[i]
+    for(l=99; l > 0; l--){
+        let minusOne = l - 1;
+        if(l >= 1){
+            console.log(l + " lines of code in the file, "
+             + l + " lines of code; " + friend + " strikes one out, "
+             + "clears it all out, " + minusOne + " lines of code in the file");
+             if(minusOne = 1){
+                 console.log("No more lines of code in the file!")
+             }   
+        }
+
+    }
+}
