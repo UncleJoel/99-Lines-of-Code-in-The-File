966739e9bd69693aacf7dd16e3453c755fa13343
Joel Ford <Joel@Joels-MacBook-Pro.local>
2018-10-24 14:56:59 -0500

    updated JS & REAME.md

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 // array of friends
-let friends =["Josh", "Joel", "Kelsy", "Devin", "Nathan"];
+let friends = ["Josh", "Joel", "Kelsy", "Devin", "Nathan"];
 
 //Function that logs 99 lines of code song
 
-for (i=0; i<friends.length; i++){
+for (i = 0; i < friends.length; i++) {
     console.log(friends[i])
     let friend = friends[i]
-    for(l=99; l > 0; l--){
+    for (l = 99; l > 0; l--) {
         let minusOne = l - 1;
-        if(l >= 1){
+        if (l > 1) {
             console.log(l + " lines of code in the file, "
-             + l + " lines of code; " + friend + " strikes one out, "
-             + "clears it all out, " + minusOne + " lines of code in the file");
-             if(minusOne = 1){
-                 console.log("No more lines of code in the file!")
-             }   
+                + l + " lines of code; " + friend + " strikes one out, "
+                + "clears it all out, " + minusOne + " lines of code in the file");
+        } else { 
+            console.log(l + " lines of code in the file, "
+                + l + " lines of code; " + friend + " strikes one out, "
+                + "clears it all out, " + "no more lines of code in the file!");
         }
-
     }
+
 }
